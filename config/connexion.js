48826fb4b58f4fe1436d3d65a51c08f018006f5a
said2715374 <2715374@gmail.com>
2024-03-02 21:12:36 -0500

    creation de la base de donnee

---
+++ config/connexion.js
@@ -0,0 +1,10 @@
+
+import mysql from 'promise-mysql'
+const connexion = mysql.createPool({
+connectionLimit: process.env.DB_CONNECTION_LIMIT,
+host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME
+})
+export default connexion
